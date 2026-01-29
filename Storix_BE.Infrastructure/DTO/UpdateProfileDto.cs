625592c57afa41302e74f9abfd78bd7ffb1840fd
MinhNhatTyson <minhnhat0132@gmail.com>
2026-01-29 17:23:00 +0700

    a

---
+++ Storix_BE.Infrastructure/DTO/UpdateProfileDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storix_BE.Repository.DTO
+{
+    public class UpdateProfileDto
+    {
+        public int? CompanyId { get; set; }
+
+        public string? FullName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+        public string? PasswordHash { get; set; }
+    }
+}
